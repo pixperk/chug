59d5954be0efa40bdafdab0e128b798aa2ebe3a6
Yashaswi Kumar Mishra <mishrayashaswikumar@gmail.com>
2025-06-22 10:10:47 +0530

    feat : add config for variables

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,34 @@
 package config
+
+import (
+	"errors"
+	"os"
+
+	"gopkg.in/yaml.v2"
+)
+
+type Config struct {
+	PostgresURL   string `yaml:"pg_url"`
+	ClickHouseURL string `yaml:"ch_url"`
+	Table         string `yaml:"table"`
+	Limit         int    `yaml:"limit"`
+	BatchSize     int    `yaml:"batch_size"`
+}
+
+func Load(path string) (*Config, error) {
+	if path == "" {
+		path = ".chug.yaml"
+	}
+
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return nil, errors.New("config file not found, please provide --config flag or create .chug.yaml")
+	}
+
+	var config Config
+	if err := yaml.Unmarshal(data, &config); err != nil {
+		return nil, errors.New("failed to parse config file: " + err.Error())
+	}
+
+	return &config, nil
+}
